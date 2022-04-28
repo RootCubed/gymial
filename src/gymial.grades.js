4e8e71c579d9be9d27f44b0a8f8dc66f5b0fcdca
RootCubed <liam@rootcubed.dev>
2022-04-28 21:10:03 +0200

    Grade editor refactoring

--- src/gymial.grades.js
+++ src/gymial.grades.js
@@ -2,7 +2,15 @@ import { $c, $i, $sa, $esc } from "./gymial.helper.js";
 
 import { getGradeData } from "./gymial.store.js";
 
-let viewPluspoints = false;
+import * as templ from "./gymial.templates.js";
+
+let viewState = {
+    viewPluspoints: false
+};
+
+export function init() {
+    reloadHTML();
+}
 
 const subjectCategories = [
     "Sprachen",
@@ -137,96 +145,26 @@ const gradeColors = [
     "36FF25", // 6
 ];
 
+// helper functions
+
 const getGradeColor = grade => {
     let g = parseFloat(grade);
     return "#" + ((g < 1.0) ? getGradeColor(1.0) : (g > 6.0) ? getGradeColor(6.0) : gradeColors[(roundedAvg(g, false) - 1) * 2]);
 };
 
-let animPlaying = false;
-
-export function init() {
-    reloadHTML();
+function formatGrade(grade, isBonus) {
+    if (isBonus) return ((grade >= 0) ? "+" : "") + grade.toString();
+    if (isNaN(grade)) return "-";
+    return Math.floor(grade * 100) / 100; // floor so that e.g. 4.24999 doesn't get shown as 4.25
 }
 
-function reloadHTML() {
-    $i("main-grades-content").innerHTML = getSemestersHTML(getGradeData());
-    for (let e of $sa("#main-grades-content .grades-sem-container")) {
-        const semName = e.dataset.name;
-        e.addEventListener("click", () => {
-            if (animPlaying) return;
-            showGradeList(getGradeData()[semName], semName, e, $i("grades-sems-cont"), "sem");
-        });
+function roundedAvg(grade, plusPoints) {
+    if (isNaN(grade) || grade == "-") return "-";
+    let rounded = Math.round(grade * 2) / 2;
+    if (plusPoints) {
+        return (rounded > 4) ? (rounded - 4) : (4 - rounded) * -2;
     }
-}
-
-function showGradeList(data, title, clickedEl, parent, type) {
-    let glEl = (type == "sem") ? getSubjList(data, title) : getGradeList(data, title, clickedEl.dataset.color, type);
-    $i("panel-grades").appendChild(glEl);
-    
-    glEl.querySelector(".grades-back-btn").addEventListener("click", () => {
-        if (animPlaying) return;
-        animPlaying = true;
-        glEl.classList.remove("visible");
-        setTimeout(() => {
-            clickedEl.classList.remove("growing");
-            glEl.remove();
-            setTimeout(() => {
-                parent.classList.remove("growing");
-                animPlaying = false;
-            }, 250);
-        }, 100);
-    });
-
-    glEl.querySelector(".grades-more-btn").addEventListener("click", () => {
-        glEl.querySelector(".grades-more-btn").classList.toggle("active");
-        glEl.querySelector(".grades-dropdown-more").classList.toggle("hidden");
-    });
-
-    glEl.querySelector(".grades-delete-link").addEventListener("click", () => {
-        if (confirm("Wirklich löschen?")) {
-            // TODO
-        }
-    });
-
-    for (let child of glEl.getElementsByClassName("grades-overview-container")) {
-        child.addEventListener("click", () => {
-            let t = child;
-            if (t.classList.contains("grades-grade-container") && !t.classList.contains("grades-subgrade")) {
-                alert("grade editor!");
-                return;
-            }
-            if (t.dataset.name) {
-                showGradeList(data[t.dataset.name], t.dataset.name, t, glEl, "subj");
-            } else if (t.dataset.index) {
-                showGradeList(data[t.dataset.index].value, data[t.dataset.index].title, t, glEl, "grade");
-            }
-        }, false);
-    }
-
-    animPlaying = true;
-    parent.classList.add("growing");
-    clickedEl.classList.add("growing");
-    setTimeout(() => {
-        glEl.classList.add("visible");
-        animPlaying = false;
-    }, 100);
-}
-
-function getGradeOverviewContainerHTML(topClass, title, avg, plusPoints, color) {
-    avg = Math.floor(avg * 100) / 100; // floor so that e.g. 4.24999 doesn't get shown as 4.25
-    let rounded = roundedAvg(avg, plusPoints);
-    if (isNaN(avg)) {
-        avg = "-";
-        rounded = "-";
-    }
-    return `
-<div class="grades-overview-container ${topClass}" data-name="${$esc(title)}" data-color="${color}">
-    <span class="grades-overview-span grades-overview-title">${$esc(title)}</span>
-    <span class="grades-overview-span grades-overview-avg has-avg-label">${avg}</span>
-    <span class="grades-overview-span grades-overview-round-avg">${rounded}</span>
-    <div class="grades-overview-vertbar" style="background-color: ${getGradeColor(avg)}"></div>
-</div>
-`;
+    return rounded;
 }
 
 function weightAsString(grade) {
@@ -243,148 +181,6 @@ function weightAsString(grade) {
         case "perc_entire":
             return "Gewicht: " + Math.round(weight * 100 * 100) / 100 + "% der Zeugnisnote";
     }
-}
-
-function getGradeContainerHTML(grade, index, color) {
-    // use calculateGradesAvg in case it is a subgrade
-    let gradeAvg = calculateGradesAvg([{
-        grade_type: grade.grade_type,
-        value: grade.value,
-        weight_type: "fullgrade",
-        weight: 1
-    }]);
-    let rounded = Math.floor(gradeAvg * 100) / 100;
-    if (isNaN(rounded)) {
-        rounded = "-";
-    }
-    if (grade.grade_type == "bonus" && grade.value >= 0) {
-        rounded = "+" + grade.value;
-    }
-    return `
-<div class="grades-overview-container grades-grade-container ${(grade.grade_type == "subgrade") ? "grades-subgrade" : ""}"
-data-index="${index}" data-color="${color}">
-    <span class="grades-overview-span grades-overview-title">${$esc(grade.title)}</span>
-    <span class="grades-overview-span grades-overview-weight">${weightAsString(grade)}</span>
-    <span class="grades-overview-span grades-overview-grade">${rounded}</span>
-    <div class="grades-overview-vertbar" style="background-color: ${getGradeColor(rounded)}"></div>
-</div>
-`;
-}
-
-function getSemestersHTML(data) {
-    let html = `
-<div class="grades-overview-container grades-add-sem">
-    <span class="grades-overview-span">+</span>
-</div>
-`;
-    for (let sem in data) {
-        html += getGradeOverviewContainerHTML("grades-sem-container", sem, calculateSemAvg(data[sem]), viewPluspoints);
-    }
-    return html;
-}
-
-function getSubjList(data, title) {
-    let subjGroups = subjectCategories.map(e => ({"title": e, "grades": [], "html": ""}));
-    for (let subj in data) {
-        let catName = (subjects[subj]) ? subjects[subj].category : "Andere";
-        let categoryID = subjectCategories.indexOf(catName);
-        subjGroups[categoryID].grades.push(data[subj]);
-        subjGroups[categoryID].html += getGradeOverviewContainerHTML("grades-subj-container", subj, calculateGradesAvg(data[subj]), viewPluspoints, categoryID);
-    }
-    let avg = Math.floor(calculateSemAvg(data) * 100) / 100;
-    let html = `
-<div class="grades-back-btn">&#xd7;</div>
-<span class="grades-more-btn">
-    <svg width="40" height="40">
-        <circle cx="8" cy="20" r="3" />
-        <circle cx="20" cy="20" r="3" />
-        <circle cx="32" cy="20" r="3" />
-    </svg>
-</span>
-<div class="grades-dropdown-more hidden">
-<span class="grades-link">${"Semester"} umbenennen</span>
-<span class="grades-link grades-delete-link">${"Semester"} löschen</span>
-</div>
-<h2 class="grades-heading">${title} (⌀ ${avg})</h2>
-<div>
-    <div class="grades-overview-container grades-add-grade">
-        <span class="grades-overview-span">+ Subject</span>
-    </div>
-</div>
-`;
-    for (let i = 0; i < subjGroups.length; i++) {
-        let subjGroup = subjGroups[i];
-        if (subjGroup.grades.length > 0) {
-            let groupAvg = Math.floor(calculateGradesAvg(subjGroup.grades.map(grade => {
-                return {
-                    "title": "",
-                    "grade_type": "subgrade",
-                    "weight_type": "fullgrade",
-                    "weight": 1,
-                    "value": grade
-                };
-            })) * 100) / 100;
-            if (isNaN(groupAvg)) groupAvg = "-";
-            html += `
-<div class="grades-group-cont grades-sg${i}">
-    <div class="grades-group-title">
-        <span class="grades-group-name">${subjGroup.title}</span>
-        <span class="grades-group-avg has-avg-label">${groupAvg}</span>
-    </div>
-    ${subjGroup.html}
-</div>      
-`;
-        }
-    }
-    let divEl = document.createElement("div");
-    divEl.classList.add("grades-cont");
-    divEl.innerHTML = html;
-    return divEl;
-}
-
-function getGradeList(data, title, colorname, type) {
-    let avg = Math.floor(calculateGradesAvg(data) * 100) / 100;
-    let html = `
-<div class="grades-back-btn">&#xd7;</div>
-<span class="grades-more-btn">
-    <svg width="40" height="40">
-        <circle cx="8" cy="20" r="3" />
-        <circle cx="20" cy="20" r="3" />
-        <circle cx="32" cy="20" r="3" />
-    </svg>
-</span>
-<div class="grades-dropdown-more hidden">
-    <span class="grades-link">${(type == "subj") ? "Fach" : "Note"} umbenennen</span>
-    <span class="grades-link grades-delete-link">${(type == "subj") ? "Fach" : "Note"} löschen</span>
-</div>
-<h2 class="grades-heading">${title} (⌀ ${avg})</h2>
-<div class="grades-content">
-    <div style="width: 100%;">
-        <div class="grades-overview-container grades-add-grade">
-            <span class="grades-overview-span">+ Grade</span>
-        </div>
-        <div class="grades-overview-container grades-add-folder">
-            <span class="grades-overview-span">+ Folder</span>
-        </div>
-    </div>
-`;
-    for (let i = 0; i < data.length; i++) {
-        let grade = data[i];
-        html += getGradeContainerHTML(grade, i, colorname);
-    }
-    html += "</div>";
-    let divEl = document.createElement("div");
-    divEl.classList.add("grades-cont", "grades-sg" + colorname);
-    divEl.innerHTML = html;
-    return divEl;
-}
-
-function roundedAvg(grade, plusPoints) {
-    let rounded = Math.round(grade * 2) / 2;
-    if (plusPoints) {
-        return (rounded > 4) ? (rounded - 4) : (4 - rounded) * -2;
-    }
-    return rounded;
 }
 
 function calculateSemAvg(data) {
@@ -420,8 +216,6 @@ function calculateGradesAvg(data) {
                 };
         }
     }).filter(grade => grade != null);
-
-    console.log(gradesCalc);
     
     if (gradesCalc.length == 0) return NaN;
 
@@ -454,4 +248,150 @@ function calculateGradesAvg(data) {
     }
 
     return percEntireGradesAvg * percEntireProp + regGradesAvg * (1 - percEntireProp) + bonusSum;
+}
+
+let animPlaying = false;
+
+function reloadHTML() {
+    $i("main-grades-content").innerHTML = getSemesterList(getGradeData());
+    for (let e of $sa("#main-grades-content .grades-sem-container")) {
+        const semName = e.dataset.name;
+        e.addEventListener("click", () => {
+            if (animPlaying) return;
+            showGradeList(getGradeData()[semName], semName, e, $i("grades-sems-cont"), "sem");
+        });
+    }
+}
+
+function showGradeList(data, title, clickedEl, parent, type) {
+    let glEl = (type == "sem") ? getSubjList(data, title) : getGradeList(data, title, clickedEl.dataset.color, type);
+    $i("panel-grades").appendChild(glEl);
+    
+    glEl.querySelector(".grades-back-btn").addEventListener("click", () => {
+        if (animPlaying) return;
+        animPlaying = true;
+        glEl.classList.add("fade-out");
+        clickedEl.classList.remove("growing");
+        setTimeout(() => {
+            glEl.remove();
+            parent.classList.remove("growing");
+            animPlaying = false;
+        }, 250);
+    });
+
+    glEl.querySelector(".grades-more-btn").addEventListener("click", () => {
+        glEl.querySelector(".grades-more-btn").classList.toggle("active");
+        glEl.querySelector(".grades-dropdown-more").classList.toggle("hidden");
+    });
+
+    glEl.querySelector(".grades-delete-link").addEventListener("click", () => {
+        if (confirm("Wirklich löschen?")) {
+            // TODO
+        }
+    });
+
+    for (let child of glEl.getElementsByClassName("grades-overview-container")) {
+        child.addEventListener("click", () => {
+            let t = child;
+            if (t.classList.contains("grades-grade-container") && !t.classList.contains("grades-subgrade")) {
+                alert("grade editor!");
+                return;
+            }
+            if (t.dataset.index) {
+                showGradeList(data[t.dataset.index].value, data[t.dataset.index].title, t, glEl, "grade");
+            } else if (t.dataset.name) {
+                showGradeList(data[t.dataset.name], t.dataset.name, t, glEl, "subj");
+            }
+        }, false);
+    }
+
+    animPlaying = true;
+    parent.classList.add("growing");
+    clickedEl.classList.add("growing");
+    setTimeout(() => { animPlaying = false; }, 250);
+}
+
+function genGradeOverviewContainer(topClass, title, avg, plusPoints) {
+    let fAvg = formatGrade(avg, false);
+    let rAvg = roundedAvg(avg, plusPoints);
+    return templ.gradeSubjContainer(topClass, $esc(title), fAvg, rAvg, getGradeColor(avg));
+}
+
+function genGradeContainer(grade, index) {
+    // use calculateGradesAvg in case it is a subgrade
+    let gradeAvg = calculateGradesAvg([{
+        grade_type: grade.grade_type,
+        value: grade.value,
+        weight_type: "fullgrade",
+        weight: 1
+    }]);
+    let fAvg = formatGrade(gradeAvg, grade.grade_type == "bonus");
+    let subgradeClass = (grade.grade_type == "subgrade") ? "grades-subgrade" : "";
+    return templ.gradeGradeContainer(
+        "grades-grade-container " + subgradeClass, index,
+        $esc(grade.title), weightAsString(grade), fAvg, getGradeColor(fAvg)
+    );
+}
+
+function getSemesterList(data) {
+    let html = `
+<div class="grades-overview-container grades-add-sem">
+    <span class="grades-overview-span">(Placeholder) +Semester</span>
+</div>
+`;
+    for (let sem in data) {
+        html += genGradeOverviewContainer("grades-sem-container", sem, calculateSemAvg(data[sem]), viewState.viewPluspoints);
+    }
+    return html;
+}
+
+function getSubjList(data, title) {
+    let subjGroups = subjectCategories.map(e => ({"title": e, "grades": [], "html": ""}));
+    for (let subj in data) {
+        let catName = (subjects[subj]) ? subjects[subj].category : "Andere";
+        let categoryID = subjectCategories.indexOf(catName);
+        subjGroups[categoryID].grades.push(data[subj]);
+        subjGroups[categoryID].html += genGradeOverviewContainer("grades-subj-container", subj, calculateGradesAvg(data[subj]), viewState.viewPluspoints, categoryID);
+    }
+    let fAvg = formatGrade(calculateSemAvg(data), false);
+
+    let html = "";
+    for (let i = 0; i < subjGroups.length; i++) {
+        let subjGroup = subjGroups[i];
+        if (subjGroup.grades.length > 0) {
+            let groupAvg = formatGrade(calculateGradesAvg(subjGroup.grades.map(grade => {
+                return {
+                    "title": "",
+                    "grade_type": "subgrade",
+                    "weight_type": "fullgrade",
+                    "weight": 1,
+                    "value": grade
+                };
+            })), false);
+            html += templ.gradeGroup(subjGroup.title, groupAvg, subjGroup.html);
+        }
+    }
+    let divEl = document.createElement("div");
+    divEl.classList.add("grades-cont");
+    divEl.innerHTML = templ.gradeList([
+        { title: "Semester umbenennen", cl: "grades-rename-link" },
+        { title: "Semester löschen", cl: "grades-delete-link" }
+    ], title, fAvg, "sem", html);
+    return divEl;
+}
+
+function getGradeList(data, title, colorname, type) {
+    let fAvg = formatGrade(calculateGradesAvg(data), false);
+    let html = "";
+    for (let i = 0; i < data.length; i++) {
+        let grade = data[i];
+        html += genGradeContainer(grade, i);
+    }
+    let divEl = document.createElement("div");
+    divEl.classList.add("grades-cont", "grades-sg" + colorname);
+    divEl.innerHTML = templ.gradeList([
+        { title: "Semester umbenennen", cl: "grades-rename-link" },
+        { title: "Semester löschen", cl: "grades-delete-link" }
+    ], title, fAvg, "subj", html);
+    return divEl;
 }
---
+++ src/gymial.templates.js
@@ -0,0 +1,74 @@
+const gradeContainerGeneric = (className, title, vertbarCol, content, index) => `
+<div class="grades-overview-container ${className}" data-name="${title}"
+${(index != undefined) ? "data-index=\"" + index + "\"" : ""}>
+    <span class="grades-overview-span grades-overview-title">${title}</span>
+    ${content}
+    <div class="grades-overview-vertbar" style="background-color: ${vertbarCol}"></div>
+</div>
+`;
+
+export const gradeSubjContainer = (className, title, trueAvg, roundedAvg, vertbarCol) => {
+    return gradeContainerGeneric(className, title, vertbarCol, `
+        <span class="grades-overview-span grades-overview-avg has-avg-label">${trueAvg}</span>
+        <span class="grades-overview-span grades-overview-round-avg">${roundedAvg}</span>
+    `);
+};
+
+export const gradeGradeContainer = (className, index, title, weight, roundedAvg, vertbarCol) => {
+    return gradeContainerGeneric(className, title, vertbarCol, `
+        <span class="grades-overview-span grades-overview-weight">${weight}</span>
+        <span class="grades-overview-span grades-overview-grade">${roundedAvg}</span>
+    `, index);
+};
+
+const gradeListPre = (links, title, avg) => `
+<div class="grades-back-btn">&#xd7;</div>
+<span class="grades-more-btn">
+    <svg width="40" height="40">
+        <circle cx="8" cy="20" r="3" />
+        <circle cx="20" cy="20" r="3" />
+        <circle cx="32" cy="20" r="3" />
+    </svg>
+</span>
+<div class="grades-dropdown-more hidden">
+    ${links.map(e => {
+        return `<span class="grades-link ${e.cl}">${e.title}</span>`;
+    }).join("")}
+</div>
+<h2 class="grades-heading">${title} (⌀ ${avg})</h2>
+`;
+
+const gradeListAddForSem = `
+<div class="grades-overview-container grades-add-grade">
+    <span class="grades-overview-span">(Placeholder) +Subject</span>
+</div>
+`;
+
+const gradeListAddForSubj = `
+<div class="grades-overview-container grades-add-grade">
+    <span class="grades-overview-span">(Placeholder) +Grade</span>
+</div>
+<div class="grades-overview-container grades-add-folder">
+    <span class="grades-overview-span">(Placeholder) +Folder</span>
+</div>
+`;
+
+export const gradeList = (links, title, avg, type, content) => `
+${gradeListPre(links, title, avg)}
+${(type == "sem") ? "" : "<div class=\"grades-content\">"}
+    <div style="width: 100%;">
+        ${(type == "sem") ? gradeListAddForSem : gradeListAddForSubj}
+    </div>
+    ${content}
+${(type == "sem") ? "" : "</div>"}
+`;
+
+export const gradeGroup = (title, avg, content) => `
+<div class="grades-group-cont">
+    <div class="grades-group-title">
+        <span class="grades-group-name">${title}</span>
+        <span class="grades-group-avg has-avg-label">${avg}</span>
+    </div>
+    ${content}
+</div> 
+`;
