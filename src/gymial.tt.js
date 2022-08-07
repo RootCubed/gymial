253fff3179b7561f8b04790bda8e00fdbe87572c
RootCubed <liam@rootcubed.dev>
2022-08-07 13:39:48 +0200

    Remove next semester online message

--- src/gymial.tt.js
+++ src/gymial.tt.js
@@ -18,7 +18,7 @@ const ERROR_VIEWCACHE = "Das TAM-Intranet ist momentan leider offline. Klicke <a
 " um die zuletzt geladene Version dieses Studenplans anschauen ";
 
 const NEXT_SEM_START = 1661119200000;
-const nextSemOnline = true;
+const nextSemOnline = false;
 
 let times = timesPre73Post76;
 let shortTimes = shortTimesPre73Post76;
